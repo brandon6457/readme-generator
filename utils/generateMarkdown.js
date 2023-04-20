e49bd144b1b8e37c70399a222012aa3496cbac5d
brandon6457 <brandon6457@gmail.com>
2023-04-20 17:21:48 -0400

    added some more content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+   return `https://img.shields.io/badge/license-${license}-blue`
+ }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+   return `https://opensource.org/licenses/-${license}-`
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(description, installation, usage, credits, license, features, contributions, tests, github, email) {
-   return `# <Your-Project-Title>
+function generateMarkdown(title, description, installation, usage, credits, license, features, contributions, tests, github, email) {
+   return `# ${title}
+   ${renderLicenseBadge(license)}
   ## Description
   ${description}
   ## Table of Contents (Optional)
@@ -23,13 +28,13 @@ function generateMarkdown(description, installation, usage, credits, license, fe
   - [License](#license)
   
   ## Installation
-  ${nstallation}
+  ${installation}
   ## Usage
   ${usage}
   ## Credits
   ${credits}
   ## License
-  ${license}
+  ${renderLicenseLink(license)}
   ## Features
   ${features}
   ## How to Contribute
